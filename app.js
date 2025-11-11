4b9c62572f674c5d4d4af666305b2783a38e1111
Sanketh S G <sankethsg2944@gmail.com>
2025-11-11 12:27:50 +0100

    Enhance deployment pipeline with build step and trigger deployment; add build script

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
+app.get('/version', (req, res) => {
+  res.send('Pokedex version: 1.0.0')
+})
+
+app.get('/healthz', (req, res) => {
+  res.send('OK')
+})
+
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`)
 })
